6e8a34a444c2e17bb2f700a9e293c0fb5d8e6c14
marharyta18 <margolu18@gmai.com>
2023-01-18 12:08:16 +0100

    13hw

--- 13/js/train.js
+++ 13/js/train.js
@@ -1,2 +1,17 @@
-let mess = "Enter code";
-let a = prompt (mess);
+let mess = "Enter num of seat:";
+let seat = prompt (mess);
+
+// купе
+//let coupe = seat <= 36 ? (seat-1)/4+1 : (54-seat)/2+1;
+let coupe = seat <= 36 ? Math.trunc((seat-1)/4+1) : Math.trunc((54-seat)/2+1);
+
+//верхнє чи нижнє
+let floor = seat%2 == 0 ? "on the top" : "on the bottom";
+
+//бічне чи ні
+let side = seat<=36 ? "in coupe" : "aside";
+
+
+let res = `Your seat ${floor}, coupe ${coupe}, ${side}.`;
+
+alert(res);
